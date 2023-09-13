d9d84134389b5db2002d589f2d5175b16bedd53f
bobobobo3 <144717421+bobobobo3@users.noreply.github.com>
2023-09-13 08:41:49 -0700

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,21 @@ def main():
     """
     code your program here 
     """
+      num1 = random.randint(0,100)
+    num2 = random.randint(0,100)
+    num3 = random.randint(0,100)
 
+    if num1 < num3:
+        if num1 < num2:
+            minval = num1
+        else:
+            minval = num2
+    else:
+        if num3 < num2:
+            minval = num3 
+        else:
+            minval = num2
+    print (num1, num2, num3, minval)
    ################################################## 
    # Do not delete the return statement 
    ################################################## 
